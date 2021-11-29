46fef6819b6c3bbfd3d883262fb1673115b05c63
maurywilde <mauricio.wilde@digitalonus.com>
2021-11-28 19:33:10 -0600

    tf format

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,18 +15,18 @@ provider "azurerm" {
 
 provider "helm" {
   kubernetes {
-    host                   = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
-    client_certificate     = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
+    host                   = data.azurerm_kubernetes_cluster.example.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)
   }
 }
 
 provider "kubernetes" {
-  host                   = "${data.azurerm_kubernetes_cluster.example.kube_config.0.host}"
-  client_certificate     = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
+  host                   = data.azurerm_kubernetes_cluster.example.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)
 }
 
 terraform {
@@ -68,13 +68,13 @@ module "aks" {
 
 #Helm
 module "helm" {
-  source                 = "./modules/helm"
-  prefix                 = var.prefix
-  location               = var.location
-  client_id              = var.client_id
-  client_secret          = var.client_secret
-  node_count             = var.node_count
-  resource_group_name    = data.azurerm_resource_group.rg.name
+  source              = "./modules/helm"
+  prefix              = var.prefix
+  location            = var.location
+  client_id           = var.client_id
+  client_secret       = var.client_secret
+  node_count          = var.node_count
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 #Creation of load balancer to add a little extra
